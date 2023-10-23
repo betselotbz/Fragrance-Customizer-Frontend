534dd1a8cfd458698e7e0b5bc5e1d78a4f94542f
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-23 14:48:03 -0500

    Create landing component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PerfumeComponent } from './perfume/perfume.component';
+import { CustomFragranceComponent } from './custom-fragrance/custom-fragrance.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { LandingComponent } from './landing/landing.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PerfumeComponent,
+    CustomFragranceComponent,
+    IngredientComponent,
+    LandingComponent,
   ],
   imports: [
     BrowserModule
