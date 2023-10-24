2ec35410ebac48d077667f50876f5e3d68b89683
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-24 13:35:27 -0500

    Set up Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { CarouselComponent } from './homepage/carousel/carousel.component'; 
+import { CarouselComponent } from './homepage/carousel/carousel.component';
+import { FooterComponent } from './shared/footer/footer.component'; 
 
 
 
@@ -13,7 +14,8 @@ import { CarouselComponent } from './homepage/carousel/carousel.component';
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomepageComponent
+    HomepageComponent,
+    FooterComponent, 
   ],
   imports: [
     BrowserModule,
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,12 @@
 // homepage/homepage.component.ts
 import { Component } from '@angular/core';
+import { CarouselComponent } from './carousel/carousel.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
 })
+
 export class HomepageComponent {}
