d83129456e8392a869741de3137cf71f09822093
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-23 23:00:55 -0500

    Create navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PerfumeComponent } from './perfume/perfume.component';
-import { CustomFragranceComponent } from './custom-fragrance/custom-fragrance.component';
-import { IngredientComponent } from './ingredient/ingredient.component';
-import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PerfumeComponent,
-    CustomFragranceComponent,
-    IngredientComponent,
-    LandingComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule
