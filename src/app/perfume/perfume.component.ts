898ae6870580f7c70354502072d2c4118342b648
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-25 09:12:36 -0500

    Implement Perfume component html and component ts

--- src/app/perfume/perfume.component.ts
+++ src/app/perfume/perfume.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+//perfume.component.ts
+import { Component, OnInit } from '@angular/core';
+import { PerfumeService } from './perfume.service';
 
 @Component({
   selector: 'app-perfume',
   templateUrl: './perfume.component.html',
-  styleUrls: ['./perfume.component.css']
+  styleUrls: ['./perfume.component.css'],
 })
-export class PerfumeComponent {
+export class PerfumeComponent implements OnInit {
   perfumes: any[] = [];
+
+  // Mock data to use until the actual data is fetched
+  mockPerfumes: any[] = [
+    {
+      name: 'Chanel No. 5',
+      description: 'Classic floral fragrance with notes of jasmine and rose.',
+      image: 'assets/Image/perfume4.webp',
+    },
+    {
+      name: 'Chanel No. 5',
+      description: 'Classic floral fragrance with notes of jasmine and rose.',
+      image: 'assets/Image/perfume4.webp',
+    },
+    {
+      name: 'Chanel No. 5',
+      description: 'Classic floral fragrance with notes of jasmine and rose.',
+      image: 'assets/Image/perfume4.webp',
+    },
+    {
+      name: 'Chanel No. 5',
+      description: 'Classic floral fragrance with notes of jasmine and rose.',
+      image: 'assets/Image/perfume4.webp',
+    },
+    {
+      name: 'Chanel No. 5',
+      description: 'Classic floral fragrance with notes of jasmine and rose.',
+      image: 'assets/Image/perfume4.webp',
+    },
+    {
+      name: 'Chanel No. 5',
+      description: 'Classic floral fragrance with notes of jasmine and rose.',
+      image: 'assets/Image/perfume4.webp',
+    },
+
+  ];
+
+  constructor(private perfumeService: PerfumeService) {}
+
+  ngOnInit(): void {
+    // Initially, use the mock data
+    this.perfumes = this.mockPerfumes;
+
+    // Fetch perfumes data from the service
+    this.perfumeService.getPerfumes().subscribe((data) => {
+      // Once the actual data is fetched, I can replace the mock data
+      this.perfumes = data;
+    });
+  }
 }
