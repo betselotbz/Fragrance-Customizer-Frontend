2679903d0305c23e1f09240edbce5cf178f15668
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-25 16:58:17 -0500

    Refactor navbar html and ts to make login and logout responsive

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {AuthenticationService} from "../../authentication.service";
+import {Subscription} from "rxjs";
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy{
+  isLoggedIn: boolean= true;
+  private subscription: Subscription;
+  onClickLogout() {
+    this.authService.setUserLoggedIn(false)
+    this.authService.removeToken()
+    console.log(9879)
+  };
+  constructor(private authService: AuthenticationService) {
+    this.subscription = this.authService.userLoggedIn$.subscribe(
+      loggedIn => this.isLoggedIn = loggedIn
+    );
+  }
+
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
 }
