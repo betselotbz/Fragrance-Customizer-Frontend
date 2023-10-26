f3f41c8e748f7ab4a344ed22e8f1dcd8fd65c722
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-26 14:14:56 -0500

    Implement Filter method in perfume component ts and add styling

--- src/app/perfume/perfume.component.ts
+++ src/app/perfume/perfume.component.ts
@@ -1,6 +1,16 @@
-//perfume.component.ts
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { PerfumeService } from './perfume.service';
+import { CartService } from '../cart/cart.service';
+
+export type Perfume = {
+  "id": number,
+  "name": string,
+  "family": string,
+  "notes": string,
+  "image": string,
+  "userCartItems": any[]
+}
 
 @Component({
   selector: 'app-perfume',
@@ -8,53 +18,64 @@ import { PerfumeService } from './perfume.service';
   styleUrls: ['./perfume.component.css'],
 })
 export class PerfumeComponent implements OnInit {
-  perfumes: any[] = [];
+  perfumes: Perfume[] = [];
+  displayedPerfumes: Perfume[] = [];
+  cartOpen: boolean = false;
 
-  // Mock data to use until the actual data is fetched
-  mockPerfumes: any[] = [
-    {
-      name: 'Chanel No. 5',
-      description: 'Classic floral fragrance with notes of jasmine and rose.',
-      image: 'assets/Image/perfume4.webp',
-    },
-    {
-      name: 'Chanel No. 5',
-      description: 'Classic floral fragrance with notes of jasmine and rose.',
-      image: 'assets/Image/perfume4.webp',
-    },
-    {
-      name: 'Chanel No. 5',
-      description: 'Classic floral fragrance with notes of jasmine and rose.',
-      image: 'assets/Image/perfume4.webp',
-    },
-    {
-      name: 'Chanel No. 5',
-      description: 'Classic floral fragrance with notes of jasmine and rose.',
-      image: 'assets/Image/perfume4.webp',
-    },
-    {
-      name: 'Chanel No. 5',
-      description: 'Classic floral fragrance with notes of jasmine and rose.',
-      image: 'assets/Image/perfume4.webp',
-    },
-    {
-      name: 'Chanel No. 5',
-      description: 'Classic floral fragrance with notes of jasmine and rose.',
-      image: 'assets/Image/perfume4.webp',
-    },
-
-  ];
-
-  constructor(private perfumeService: PerfumeService) {}
+  constructor(
+    private perfumeService: PerfumeService,
+    private cartService: CartService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    // Initially, use the mock data
-    this.perfumes = this.mockPerfumes;
+    this.route.params.subscribe((params: Params) => {
+      const family = params['family'];
+      const id = params['id'];
 
-    // Fetch perfumes data from the service
-    this.perfumeService.getPerfumes().subscribe((data) => {
-      // Once the actual data is fetched, I can replace the mock data
-      this.perfumes = data;
+      if (family) {
+        this.perfumeService.getPerfumesByFamily(family).subscribe((data) => {
+          this.perfumes = data;
+          this.displayedPerfumes = data; // Initially, display all perfumes for the family
+        });
+      } else if (id) {
+        this.perfumeService.getPerfumeById(id).subscribe((data) => {
+          this.perfumes = [data];
+          this.displayedPerfumes = [data]; // Display the single perfume
+        });
+      } else {
+        this.perfumeService.getPerfumes().subscribe((data) => {
+          this.perfumes = data;
+          this.displayedPerfumes = data; // Initially, display all perfumes
+        });
+      }
     });
   }
+
+  toggleCart() {
+    this.cartOpen = !this.cartOpen;
+  }
+
+  closeCart() {
+    this.cartOpen = false;
+  }
+
+  onClickAddToCart(perfume: Perfume) {
+    this.cartService.createCartItem(perfume.id, 'perfume').subscribe(data => {
+      console.log(data);
+    });
+    console.log(perfume);
+  }
+
+  filterByFamily(family: string): void {
+    if (family === 'all') {
+      // If 'all' is selected, show all perfumes
+      this.displayedPerfumes = this.perfumes;
+    } else {
+      // Filter perfumes based on the selected family
+      this.displayedPerfumes = this.perfumes.filter(perfume => perfume.family === family);
+    }
+  }
+
 }
+
