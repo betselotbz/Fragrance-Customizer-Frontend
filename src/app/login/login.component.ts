5fa95c4913ccfcf7c3103e1fc93e5bb62d8ad2ef
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-27 03:57:24 -0500

    Update Minor Styling in Navbar and Footer

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,38 +4,38 @@ import {AuthenticationService} from '../authentication.service';
 import {Router} from "@angular/router";
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css'],
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-    email: string = '';
-    password: string = '';
+  email: string = '';
+  password: string = '';
 
-    constructor(private authService: AuthenticationService, private router: Router) {
-    }
+  constructor(private authService: AuthenticationService, private router: Router) {
+  }
 
-    login(): void {
-        this.authService.login(this.email, this.password).subscribe(
-            (response: any) => {
-                const token = response.jwt; // Adjust the property name based on API response
-                this.authService.setAuthToken(token);
-                console.log('Login successful:', response);
-                this.authService.setUserLoggedIn(true)
+  login(): void {
+    this.authService.login(this.email, this.password).subscribe(
+      (response: any) => {
+        const token = response.jwt; // Adjust the property name based on your API response
+        this.authService.setAuthToken(token);
+        console.log('Login successful:', response);
+        this.authService.setUserLoggedIn(true)
 
-                this.authService.getCurrentUser().subscribe(data => {
-                    this.authService.setLoggedInUser(data.data)
-                })
+        this.authService.getCurrentUser().subscribe(data => {
+          this.authService.setLoggedInUser(data.data)
+        })
 
 
-                // Handle successful login, e.g., navigate to another page
-                this.router.navigate(['/']);
-            },
-            (error: any) => {
-                console.error('Login error:', error);
-                this.authService.setUserLoggedIn(false)
-                // Handle login error, e.g., display an error message to the user
-            }
-        );
-    }
+        // Handle successful login, e.g., navigate to another page
+        this.router.navigate(['/']);
+      },
+      (error: any) => {
+        console.error('Login error:', error);
+        this.authService.setUserLoggedIn(false)
+        // Handle login error, e.g., display an error message to the user
+      }
+    );
+  }
 }
