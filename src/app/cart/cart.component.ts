fe461e89ddfe92b192c30d58f515e7c75d8d95b2
Betselot Bezabeh <betselotbz@gmail.com>
2023-10-27 01:54:48 -0500

    Refactor cart component for by adding logic for checkout and removed placeholder code

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,27 +1,71 @@
 // cart.component.ts
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserCartService} from "../userCart.service";
+import {CustomFragrance, UserCartItem} from "../shared/models";
+import {CartService} from "./cart.service";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css'],
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  cartItems: any[] = [];
+    cartItems: UserCartItem[] = [];
+    quantity: number = 1;
 
-  constructor() {}
 
-  ngOnInit(): void {
-    // Initialize or fetch cart items here
-    this.cartItems = [
-      { name: 'Item 1', price: 10.99 },
-      { name: 'Item 2', price: 19.99 },
-      // Add more items as needed
-    ];
-  }
+    constructor(private userCart: UserCartService, private cartService: CartService) {
+    }
+  // Subscribe to the userCartItemSubject to get updates on cart items
+    ngOnInit(): void {
 
-  onCheckout(): void {
-    // Handle checkout logic here
-    console.log('Checkout clicked!');
-  }
+        this.userCart.userCartItemSubject$.subscribe(data => {
+            this.cartItems = data;
+        })
+    }
+  // Handle the checkout process
+    onCheckout(): void {
+        // console.log(this.cartItems)
+
+        this.cartItems.forEach(item => {
+
+            if (item.perfume) {
+                // create item with perfume
+                this.cartService.createItem({
+                    perfume: {
+                        id: item.perfume.id
+                    },
+                    quantity: this.quantity
+                }).subscribe(data => {
+                    console.log(data)
+                })
+            }
+
+            if (item.customFragrance) {
+                // create custom fragrances and item
+                this.cartService.createCustomFragranceCartItem(item.customFragrance, this.quantity)
+            }
+
+        })
+
+        console.log('Checkout clicked!');
+    }
+
+    quantityChanged($event: Event, cartItem: UserCartItem) {
+        cartItem.quantity = Number(($event.target as unknown as any).value)
+        this.quantity = Number(($event.target as unknown as any).value)
+    }
+
+    getTotalPrice() {
+        let totalPrice = 0.0
+        this.cartItems.forEach(i => {
+            if (i.perfume?.price) {
+                totalPrice += i.perfume?.price * (i.quantity ?? 1)
+            }
+            if (i.customFragrance?.price) {
+                totalPrice += i.customFragrance?.price * (i.quantity ?? 1)
+            }
+        })
+        return totalPrice
+    }
 }
